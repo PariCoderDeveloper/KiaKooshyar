2c82b82ec15db41e3e788ee82e6970737222116a
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-08-19 11:11:00 +0330

    Chore(structure): move RateLimiting folder up one level

---
+++ Infrastructure/RateLimiting/FixedWindowRateLimitOptions.cs
@@ -0,0 +1,20 @@
+﻿using KiaKooshar.Domain.BussinessEnums;
+using System.Threading.RateLimiting;
+
+namespace KiaKooshar.Infrastructure.RateLimiting
+{
+    public sealed class RateLimitOptions
+    {
+        public FixedWindowRateLimitOptions? RefreshToken { get; set; }
+        public FixedWindowRateLimitOptions? Controller { get; set; }
+    }
+    public sealed class FixedWindowRateLimitOptions
+    {
+        public required string Name { get; set; }
+        public RateLimitScope Scope { get; set; }
+        public int PermitLimit { get; set; }
+        public TimeSpan Window { get; set; }
+        public int QueueLimit { get; set; }
+        public QueueProcessingOrder QueueProcessingOrder { get; set; }
+    }
+}
---
+++ Infrastructure/RateLimiting/RateLimitPolicy.cs
@@ -0,0 +1,8 @@
+﻿namespace KiaKooshar.Infrastructure.RateLimiting
+{
+    public static class RateLimitPolicy
+    {
+        public const string RefreshToken = "refresh-token";
+        public const string Controller = "controller";
+    }
+}
