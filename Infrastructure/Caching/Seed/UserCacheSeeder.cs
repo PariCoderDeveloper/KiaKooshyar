8afa81d5e7a870b1fabdb668b06cf42110ee27cb
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-08-21 18:30:14 +0330

    Feat (cache-seeder) : seeding all of users to cache

---
+++ Infrastructure/Caching/Seed/UserCacheSeeder.cs
@@ -0,0 +1,42 @@
+﻿using KiaKooshar.Application.Caching.Policies;
+using KiaKooshar.Application.DTOs.Identities.Cache;
+using KiaKooshar.Infrastructure.Caching.Services;
+using KiaKooshar.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace KiaKooshar.Infrastructure.Caching.Seed
+{
+    public class UserCacheSeeder
+    {
+        private readonly DatabaseContext _context;
+        private readonly RedisCacheService _cacheService;
+        public UserCacheSeeder (
+            DatabaseContext context,
+            RedisCacheService cacheService
+            )
+        {
+            _context = context;
+            _cacheService = cacheService;
+        }
+        public async Task SeedAsync (
+            CancellationToken cancellationToken = default
+            )
+        {
+            var users = await _context.Users
+                .AsNoTracking ()
+                .Select (u => new CachedUserDto
+                {
+                    Id = u.Id,
+                    Email = u.Email,
+                    Username = u.UserName
+                }).ToListAsync (cancellationToken);
+            foreach ( var user in users )
+                await _cacheService.SetAsync (
+                    $"users:{user.Id}",
+                    user,
+                    CachePolicy.Medium,
+                    cancellationToken
+                    );
+        }
+    }
+}
