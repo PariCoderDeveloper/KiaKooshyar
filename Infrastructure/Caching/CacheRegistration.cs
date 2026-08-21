9c50bcb5d04d5f39aa0d0742720f24ee3a1d8431
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-08-21 18:32:10 +0330

    Feat(cache-seeder): register cache seeder

---
+++ Infrastructure/Caching/CacheRegistration.cs
@@ -0,0 +1,19 @@
+﻿using KiaKooshar.Infrastructure.Caching.Seed;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KiaKooshar.Infrastructure.Caching
+{
+    public class CacheRegistration
+    {
+        public static async Task CacheSeeder (
+            IServiceProvider service,
+            CancellationToken cancellationToken = default
+            )
+        {
+            using var scope = service.CreateScope ();
+            var userCacheSeeder = scope.ServiceProvider
+                .GetRequiredService<UserCacheSeeder> ();
+            await userCacheSeeder.SeedAsync (cancellationToken);
+        }
+    }
+}
