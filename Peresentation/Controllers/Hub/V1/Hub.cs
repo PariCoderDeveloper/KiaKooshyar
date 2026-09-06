047458df3e7619254d5f188597dd37d44cc9a6a8
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-09-06 10:45:15 +0330

    feat (notification) : impelement notification hub

---
+++ Peresentation/Controllers/Hub/V1/Hub.cs
@@ -0,0 +1,32 @@
+﻿using KiaKooshar.Application.Features.Interfaces.SignalR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiaKooshar.Peresentation.Controllers.SignalR.V1
+{
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class Hub : ControllerBase
+    {
+        private readonly IUserNotificationService _userNotificationService;
+        public Hub (
+            IUserNotificationService userNotificationService
+            )
+        {
+            _userNotificationService = userNotificationService;
+        }
+        [Authorize]
+        public async Task<IActionResult> Notification (
+            long userId
+            )
+        {
+            await _userNotificationService
+               .NotifyForceLogoutAsync (
+                   userId.ToString (),
+                   "For some reason, you should login again"
+               );
+            return Ok ();
+        }
+
+    }
+}
