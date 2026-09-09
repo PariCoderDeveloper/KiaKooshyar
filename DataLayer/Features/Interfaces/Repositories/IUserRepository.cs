6e286e7fe7df86c89694a949d524ee96be64f09b
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-09-09 16:31:19 +0330

    feat(users): add cached user, all users, and permission methods

--- DataLayer/Features/Interfaces/Repositories/IUserRepository.cs
+++ DataLayer/Features/Interfaces/Repositories/IUserRepository.cs
@@ -1,4 +1,6 @@
 ﻿using KiaKooshar.Application.Construct.DataBases;
+using KiaKooshar.Application.DTOs.Identities.Cache;
+using KiaKooshar.Application.DTOs.Identities.Users.Queries;
 using KiaKooshar.Domain.Entities.Identity;
 using System.Linq.Expressions;
 
@@ -6,6 +8,11 @@ namespace KiaKooshar.Application.Features.Interfaces.Repositories
 {
     public interface IUserRepository : IRepository<User>
     {
+        Task<bool> HasPermission (
+            long userId,
+            string permission,
+            CancellationToken cancellationToken = default
+            );
         Task<List<string>> GetUserRoles (
             long userId,
             CancellationToken cancellationToken = default
@@ -22,5 +29,12 @@ namespace KiaKooshar.Application.Features.Interfaces.Repositories
             Expression<Func<User, bool>> wherePredicate,
             CancellationToken cancellationToken = default
             );
+        Task<CachedUserDTO?> GetCachedUserAsync (
+            long userId,
+            CancellationToken cancellationToken = default
+            );
+        IQueryable<GetAllUsersDTO?> GetAllUsersAsync (
+           CancellationToken cancellationToken = default
+           );
     }
 }
--- Infrastructure/Persistence/Repositories/Identities/UserRepository.cs
+++ Infrastructure/Persistence/Repositories/Identities/UserRepository.cs
@@ -1,4 +1,6 @@
 ﻿using KiaKooshar.Application.Construct.DataBases;
+using KiaKooshar.Application.DTOs.Identities.Cache;
+using KiaKooshar.Application.DTOs.Identities.Users.Queries;
 using KiaKooshar.Application.Features.Interfaces.Repositories;
 using KiaKooshar.Domain.Entities.Identity;
 using KiaKooshar.Infrastructure.Persistence.Repositories.Generic;
@@ -67,6 +69,76 @@ namespace KiaKooshar.Infrastructure.Persistence.Repositories.Identities
                 .FirstOrDefaultAsync (
                     cancellationToken
                     );
+        }
+        public async Task<CachedUserDTO?> GetCachedUserAsync (
+            long userId,
+            CancellationToken cancellationToken = default
+            )
+        {
+            return await _context.Users
+                .Where (u => u.Id == userId)
+                .Select (u => new CachedUserDTO
+                {
+                    Id = u.Id,
+                    Username = u.UserName,
+                    Email = u.Email,
+                    Roles = u.UserRole
+                        .Select (ur => ur.Role.Name)
+                        .Distinct ()
+                        .ToList (),
+                    RolePermissions = u.UserRole
+                        .SelectMany (ur => ur.Role.RolePermission)
+                        .Select (rp => rp.Permission.DiplayName)
+                        .Distinct ()
+                        .ToList (),
+                    Permissions = u.UserPermissions
+                        .Select (x => x.Permission.DiplayName)
+                        .ToList ()
+                })
+                .FirstOrDefaultAsync (cancellationToken);
+        }
+        public IQueryable<GetAllUsersDTO?> GetAllUsersAsync (
+            CancellationToken cancellationToken = default
+            )
+        {
+            return _context.Users
+                .Select (u => new GetAllUsersDTO
+                {
+                    Id = u.Id,
+                    UserStatus = u.Status,
+                    Email = u.Email,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    UserName = u.UserName,
+                    PhoneNumber = u.PhoneNumber,
+                    Roles = u.UserRole
+                        .Select (ur => ur.Role.Name)
+                        .Distinct ()
+                        .ToList (),
+                    Permissions = u.UserPermissions
+                        .Select (x => x.Permission.DiplayName)
+                        .ToList (),
+                    RolePermissions = u.UserRole
+                        .SelectMany (ur => ur.Role.RolePermission)
+                        .Select (rp => rp.Permission.DiplayName)
+                        .Distinct ()
+                        .ToList (),
+                }).AsQueryable ();
+        }
+
+        public async Task<bool> HasPermission (
+            long userId,
+            string permission,
+            CancellationToken cancellationToken = default
+            )
+        {
+            return await _context.Users
+                .Where (u => u.Id == userId)
+                .SelectMany (u => u.UserRole)
+                .SelectMany (ur => ur.Role.RolePermission)
+                .AnyAsync (
+                    rp => rp.Permission.DiplayName == permission,
+                    cancellationToken);
         }
     }
 }
