cfd22ceff5562e0fe0d920b6c2deedf8dcd97592
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-09-09 16:17:45 +0330

    feat (authorization) : implementing GetRolePermission and HasPermission handler

---
+++ DataLayer/Features/Identities/Admin/Handlers/Queries/HasPermission/HasPermissionHandler.cs
@@ -0,0 +1,41 @@
+﻿using KiaKooshar.Application.Construct.DataBases;
+using KiaKooshar.Application.DTOs.Common;
+using KiaKooshar.Application.Features.Identities.Admin.Requests.Queries;
+using MediatR;
+
+namespace KiaKooshar.Application.Features.Identities.Admin.Handlers.Queries.HasPermission
+{
+    public class HasPermissionHandler
+        : IRequestHandler<HasPermissionQuery, ResultDTO>
+    {
+        private readonly IUnitOfWork _unit;
+        public HasPermissionHandler (
+            IUnitOfWork unit
+            )
+        {
+            _unit = unit;
+        }
+        public async Task<ResultDTO> Handle (
+            HasPermissionQuery request,
+            CancellationToken cancellationToken = default
+            )
+        {
+            var hasPermission = await _unit.Users
+                .HasPermission (
+                    request.UserId,
+                    request.Permission,
+                    cancellationToken
+                    );
+
+            if ( !hasPermission )
+            {
+                return ResultDTO.Failure (
+                    "User doesn't have this permission."
+                    );
+            }
+
+            return ResultDTO.Success (
+                "User has this permission.");
+        }
+    }
+}
---
+++ DataLayer/Features/Identities/Admin/Requests/Queries/HasPermissionQuery.cs
@@ -0,0 +1,12 @@
+﻿using KiaKooshar.Application.DTOs.Common;
+using MediatR;
+
+namespace KiaKooshar.Application.Features.Identities.Admin.Requests.Queries
+{
+    public class HasPermissionQuery :
+        IRequest<ResultDTO>
+    {
+        public long UserId { get; set; }
+        public string Permission { get; set; } = null!;
+    }
+}
