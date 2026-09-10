4a54252e8c9f82253a8b48feab2ecbfae956e152
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-09-10 09:46:04 +0330

    feat (logout):implementing logout method

--- ClientApp/KiakosshyarApp/src/app/core/Components/layout/admin-layout/admin-layout.ts
+++ ClientApp/KiakosshyarApp/src/app/core/Components/layout/admin-layout/admin-layout.ts
@@ -7,6 +7,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../../services/auth.service';
+import { AuthStateService } from '../../../services/auth.state.service';
 
 @Component({
   selector: 'app-admin-layout',
@@ -26,8 +28,16 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrls: ['./admin-layout.css']
 })
 export class AdminLayoutComponent {
+   constructor(
+    private authService: AuthService,
+    private authStateService: AuthStateService
+  ) {}
   @ViewChild('drawer') drawer!: MatSidenav;
+  logout(): void {
+    this.authStateService.clear();
+    this.authService.logout();
 
+  }
   toggleSidenav() {
     this.drawer.toggle();
   }
