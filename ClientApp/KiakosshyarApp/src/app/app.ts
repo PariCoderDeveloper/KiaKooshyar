a3ffb773d0c1fac636f1dd8b64e751862b8ae07d
Parisa Alizadeh <parisaalizadeh13821382@gmail.com>
2026-09-10 09:48:15 +0330

    feat: clear auth state on logout

--- ClientApp/KiakosshyarApp/src/app/app.ts
+++ ClientApp/KiakosshyarApp/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, inject, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { SignalRService } from './core/services/signalr.service';
 import { AuthService } from './core/services/auth.service';
+import { AuthStateService } from './core/services/auth.state.service';
 
 @Component({
   imports: [RouterOutlet],
@@ -11,12 +12,14 @@ import { AuthService } from './core/services/auth.service';
 })
 export class App {
   protected readonly title = signal('kiakooshyarApp');
+  authStateService = inject(AuthStateService);
   signalRService = inject(SignalRService);
   authService = inject(AuthService);
   router = inject(Router)
   ngOnInit(): void {
     this.signalRService.notification$.subscribe(reason =>{
       alert(reason);
+      this.authStateService.clear();
       this.authService.logout();
       this.router.navigate(["/login"]);    
     });
